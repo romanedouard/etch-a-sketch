c12a436c26a60e46b9e72cef2f8f4f271ab8e266
Roman Edouard <123408909+romanedouard@users.noreply.github.com>
2024-04-27 19:48:02 -0400

    Add dynamic size of wrapper. Remove existing wrapper and create new wrapper each time to prevent them from stacking on each other

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
+const sizeOfGrid = 16;
 const resetButton = document.querySelector('button');
 const container = document.querySelector('.container');
 
 const createGrid = (amtOfGrids) => {
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('wrapper');
+
     for (let i = 0; i < amtOfGrids; i++) {
         const row = document.createElement('div');
         row.classList.add('grid-row');
@@ -19,9 +23,26 @@ const createGrid = (amtOfGrids) => {
             row.appendChild(gridBox);
         }
 
-        container.appendChild(row);
+        wrapper.appendChild(row);
     }
+    container.appendChild(wrapper);
 }
 
-const sizeOfGrid = 16;
-createGrid(sizeOfGrid);
+createGrid(sizeOfGrid)
+
+resetButton.addEventListener('click', () => {
+    let userSize = Number(prompt('Choose a number that is 100 or less for the dimensions of the grid:'));
+
+    while (userSize > 100) {
+        userSize = Number(prompt('Try again. Choose a number that is 100 or less:'));
+    }
+
+    const wrapper = document.querySelector('.wrapper');
+
+    if (!wrapper) {
+        createGrid(userSize);
+    } else {
+        wrapper.remove();
+        createGrid(userSize);
+    }
+})
